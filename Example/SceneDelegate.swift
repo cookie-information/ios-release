300517baf326c98ee0acbc430d846710b90a6daa
Karolina Poplawska <k.poplawska@piwik.pro>
2026-07-14 16:05:12 +0200

    [PPSDK-441] Migrate Swift example app to UIScene lifecycle

---
+++ Example/SceneDelegate.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    // The window is created by UIKit from the Main storyboard
+    // (UISceneStoryboardFile in Info.plist).
+    var window: UIWindow?
+}
